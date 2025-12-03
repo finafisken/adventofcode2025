0d18f76166435f6b79fb950b5df83eb86331c961
Markus Warne <markus.warne@gmail.com>
2025-12-03 22:04:50 +0100

    day03: part 2

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -4,10 +4,10 @@ fn main() {
     let input = fs::read_to_string("input.txt").unwrap();
 
     let p1 = part1(&input);
-    // let p2 = part2(&input);
+    let p2 = part2(&input);
 
     println!("Part 1: {}", p1);
-    // println!("Part 2: {}", p2);
+    println!("Part 2: {}", p2);
 }
 
 fn part1(data: &str) -> usize {
@@ -36,6 +36,38 @@ fn part1(data: &str) -> usize {
     max_jolts.iter().sum()
 }
 
+fn part2(data: &str) -> usize {
+    let mut max_jolts: Vec<usize> = Vec::new();
+    for line in data.lines() {
+        let numbers: Vec<_> = line.chars().map(|c| c.to_digit(10).unwrap()).collect();
+
+        let mut result_nums: Vec<String> = Vec::new();
+        let mut cursor = 0;
+
+        for i in 0..12 {
+            let slots_to_fill = 12 - i - 1;
+
+            // find first highest nr + index, leave as many slots that we still need to fill for 12char nr
+            let search_space = &numbers[cursor..numbers.len() - slots_to_fill];
+            let max = search_space.iter().max().unwrap();
+            // first position of number in search space
+            let max_idx = search_space.iter().position(|x| x == max).unwrap();
+            cursor = cursor + max_idx + 1;
+
+            result_nums.push(max.to_string());
+        }
+
+        let max_jolt: usize = result_nums.join("").parse().unwrap();
+
+        println!("m: {:?}", max_jolt);
+        max_jolts.push(max_jolt)
+    }
+
+    println!("{:?}", max_jolts);
+
+    max_jolts.iter().sum()
+}
+
 #[test]
 fn test_p1() {
     let data = r"987654321111111
@@ -51,4 +83,14 @@ fn test_p1_1() {
     let data = r"9891";
 
     assert_eq!(part1(data), 99)
+}
+
+#[test]
+fn test_p2() {
+    let data = r"987654321111111
+811111111111119
+234234234234278
+818181911112111";
+
+    assert_eq!(part2(data), 3121910778619)
 }
