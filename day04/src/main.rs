ad4e5609980fc2e108f245c40b439ebc831362cb
Markus Warne <markus.warne@gmail.com>
2025-12-04 19:22:07 +0100

    day04: part2

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -4,10 +4,10 @@ fn main() {
     let input = fs::read_to_string("input.txt").unwrap();
 
     let p1 = part1(&input);
-    // let p2 = part2(&input);
+    let p2 = part2(&input);
 
     println!("Part 1: {}", p1);
-    // println!("Part 2: {}", p2);
+    println!("Part 2: {}", p2);
 }
 
 fn part1(data: &str) -> usize {
@@ -42,6 +42,48 @@ fn part1(data: &str) -> usize {
     unblocked_count
 }
 
+fn part2(data: &str) -> usize {
+    let mut unblocked_count = 0;
+    let mut coords: HashMap<(i16, i16), char> = HashMap::new();
+    for (row_idx, line) in data.lines().enumerate() {
+        for (col_idx, char) in line.chars().enumerate() {
+            match char {
+                '@' => coords.insert((col_idx as i16, row_idx as i16), '@'),
+                _ => continue,
+            };
+        }
+    }
+
+    loop {
+        let prev_unblocked_count = unblocked_count;
+        for (x, y) in coords.keys().cloned().collect::<Vec<_>>() {
+            let mut adjecent = 0;
+
+            for y_i in (y - 1)..=(y + 1) {
+                for x_i in (x - 1)..=(x + 1) {
+                    // check if potential adjecent roll exists and is not itself
+                    if coords.contains_key(&(x_i, y_i)) && (x_i, y_i) != (x, y) {
+                        adjecent += 1;
+                    }
+                }
+            }
+
+            if adjecent < 4 {
+                unblocked_count += 1;
+                // remove roll for next run
+                coords.remove(&(x, y));
+            }
+        }
+
+        // we were not able to remove anymore, unblocked count remains the same
+        if prev_unblocked_count == unblocked_count {
+            break;
+        }
+    }
+
+    unblocked_count
+}
+
 #[test]
 fn test_p1() {
     let data = r"..@@.@@@@.
@@ -58,12 +100,18 @@ fn test_p1() {
     assert_eq!(part1(data), 13)
 }
 
-// #[test]
-// fn test_p2() {
-//     let data = r"987654321111111
-// 811111111111119
-// 234234234234278
-// 818181911112111";
+#[test]
+fn test_p2() {
+    let data = r"..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@.";
 
-//     assert_eq!(part2(data), 3121910778619)
-// }
+    assert_eq!(part2(data), 43)
+}
