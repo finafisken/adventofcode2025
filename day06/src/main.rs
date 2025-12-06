174770c4b593664f78ce0216565657c839dff006
Markus Warne <markus.warne@gmail.com>
2025-12-06 21:22:20 +0100

    day06: part2

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -1,5 +1,4 @@
-use core::num;
-use std::{cmp::max, collections::HashMap, fs};
+use std::{collections::HashMap, fs};
 
 fn main() {
     let input = fs::read_to_string("input.txt").unwrap();
@@ -47,78 +46,99 @@ fn part1(data: &str) -> usize {
 
 fn part2(data: &str) -> usize {
     #[derive(Debug)]
-    struct ProblemSpec<'a> {
-        symbol: &'a str,
+    struct ProblemSpec {
+        symbol: char,
         start: usize,
-        end: usize,
-        width: usize,
+        end: usize, // exclusive
     }
 
     let mut totals: Vec<usize> = Vec::new();
     let mut grid: Vec<Vec<char>> = Vec::new();
 
-    for row in data.lines() {
-        let cols: Vec<char> = row.chars().collect();
-        grid.push(cols)
+    // build grid and pad rows to same width
+    let lines: Vec<&str> = data.lines().collect();
+    let max_width = lines.iter().map(|l| l.len()).max().unwrap_or(0);
+
+    for row in &lines {
+        let mut cols: Vec<char> = row.chars().collect();
+        cols.resize(max_width, ' ');
+        grid.push(cols);
     }
+
+    let bottom_row = grid.pop().unwrap(); // remove last row (operators)
+
+    // check if column is a separator (all spaces in all rows)
+    let is_separator_col =
+        |col: usize| -> bool { grid.iter().all(|row| row[col] == ' ') && bottom_row[col] == ' ' };
 
     let mut specs: Vec<ProblemSpec> = Vec::new();
+    let mut in_problem = false;
+    let mut problem_start = 0;
+    let mut problem_op = ' ';
 
-    // remove last row from grid, parse to problem specification
-    for (i, char) in grid.pop().unwrap().iter().enumerate() {
-        if let Some(prev_prob) = specs.last_mut() {
-            prev_prob.end = i - 1;
-            prev_prob.width = i - prev_prob.start - 1;
+    // find problem boundaries
+    for i in 0..max_width {
+        if is_separator_col(i) {
+            if in_problem {
+                // end of current problem
+                specs.push(ProblemSpec {
+                    symbol: problem_op,
+                    start: problem_start,
+                    end: i,
+                });
+                in_problem = false;
+            }
+        } else {
+            if !in_problem {
+                // start of new problem
+                in_problem = true;
+                problem_start = i;
+                problem_op = ' ';
+            }
+
+            if bottom_row[i] == '+' || bottom_row[i] == '*' {
+                problem_op = bottom_row[i];
+            }
         }
-
-        match *char {
-            '+' => specs.push(ProblemSpec {
-                symbol: "+",
-                start: i,
-                end: 0,
-                width: 0,
-            }),
-            '*' => specs.push(ProblemSpec {
-                symbol: "*",
-                start: i,
-                end: 0,
-                width: 0,
-            }),
-            _ => (),
-        };
     }
-    println!("{:?}", specs);
+    // finalize last problem if we ended inside one
+    if in_problem {
+        specs.push(ProblemSpec {
+            symbol: problem_op,
+            start: problem_start,
+            end: max_width,
+        });
+    }
 
     for spec in specs {
-        let mut num_s: Vec<String> = vec![String::new(); spec.width];
+        let width = spec.end - spec.start;
+        let mut num_s: Vec<String> = vec![String::new(); width];
 
         for row in grid.iter() {
-            // only grab nr from our spec, start from top left
             for (x, col) in row[spec.start..spec.end].iter().enumerate() {
-                if *col == ' ' {
-                    continue;
+                if col.is_ascii_digit() {
+                    num_s[x].push(*col);
                 }
-
-                // println!("{:?}", num_s);
-                num_s[x].push(*col);
             }
         }
 
-        println!("{:?}", num_s);
+        // filter out empty columns
+        let operands: Vec<usize> = num_s
+            .iter()
+            .filter(|s| !s.is_empty())
+            .map(|s| s.parse::<usize>().unwrap())
+            .collect();
 
         let initial = match spec.symbol {
-            "*" => 1,
-            "+" => 0,
+            '*' => 1,
+            '+' => 0,
             _ => unreachable!("bad input"),
         };
 
-        let problem_sum = num_s.iter().fold(initial, |sum, val| {
-            let n = val.parse::<usize>().unwrap();
-            match spec.symbol {
-                "*" => sum * n,
-                "+" => sum + n,
-                _ => unreachable!("bad input"),
-            }
+        let problem_sum = operands.iter().fold(initial, |sum, val| match spec.symbol {
+            '*' => sum * val,
+            '+' => sum + val,
+            _ => unreachable!("bad input"),
         });
 
         totals.push(problem_sum);
